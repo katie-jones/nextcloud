a89affb6168855ef158d58a97269151b507d31ee
Katie <knjones@alumni.princeton.edu>
2020-02-25 11:12:32 +0000

    Add s3 storage as primary storage

--- nextcloud.config.php
+++ nextcloud.config.php
@@ -18,3 +18,21 @@ if (getenv('NEXTCLOUD_SYSTEM_EMAIL')) {
 
 // Add proxy settings.
 $CONFIG['trusted_proxies'] = ['127.0.0.1'];
+
+// Add spaces settings.
+if (getenv('NEXTCLOUD_S3_BUCKET')) {
+    $CONFIG['objectstore'] = array(
+            'class' => '\\OC\\Files\\ObjectStore\\S3',
+            'arguments' => array(
+                    'bucket' => getenv('NEXTCLOUD_S3_BUCKET'),
+                    'key'    => getenv('NEXTCLOUD_S3_KEY'),
+                    'secret' => getenv('NEXTCLOUD_S3_SECRET'),
+                    'hostname' => getenv('NEXTCLOUD_S3_HOSTNAME'),
+                    'region' => getenv('NEXTCLOUD_S3_REGION'),
+                    'port' => 443,
+                    'use_ssl' => true,
+                    'use_path_style'=>false,
+                    'autocreate'=>false,
+            ),
+    );
+}
