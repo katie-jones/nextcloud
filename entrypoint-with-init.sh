3e932a622ce6af6031793f8c2656ae94959d433f
Katie Jones <knjones@alumni.princeton.edu>
2022-05-07 08:59:14 +0000

    Update patch for Nextcloud 23

--- entrypoint-with-init.sh
+++ entrypoint-with-init.sh
@@ -2,47 +2,47 @@
 
 # Patch /entrypoint.sh
 patch /entrypoint.sh << EOM
-@@ -159,6 +159,40 @@
-                     if [ "$try" -gt "$max_retries" ]; then
-                         echo "installing of nextcloud failed!"
-                         exit 1
-+                    else
-+                        echo "Running post-install user initialization"
+@@ -186,6 +186,40 @@
+                         if [ "$try" -gt "$max_retries" ]; then
+                             echo "Installing of nextcloud failed!"
+                             exit 1
++                        else
++                            echo "Running post-install user initialization"
 +
-+                        # Install and enable encryption apps.
-+                        run_as "php /var/www/html/occ app:enable encryption"
-+                        run_as "php /var/www/html/occ encryption:enable"
-+                        run_as "php /var/www/html/occ app:install end_to_end_encryption"
++                            # Install and enable encryption apps.
++                            run_as "php /var/www/html/occ app:enable encryption"
++                            run_as "php /var/www/html/occ encryption:enable"
++                            run_as "php /var/www/html/occ app:install end_to_end_encryption"
 +
-+                        # Disable encrypted home storage.
-+                        run_as "php /var/www/html/occ config:app:set encryption encryptHomeStorage --value 0"
++                            # Disable encrypted home storage.
++                            run_as "php /var/www/html/occ config:app:set encryption encryptHomeStorage --value 0"
 +
-+                        # Install and enable 2FA app.
-+                        run_as "php /var/www/html/occ app:install twofactor_totp"
++                            # Install and enable 2FA app.
++                            run_as "php /var/www/html/occ app:install twofactor_totp"
 +
-+                        # Enable external storage and create mounts.
-+                        run_as "php /var/www/html/occ app:enable files_external"
-+                        if [ -f /opt/nextcloud/custom-mounts.json ]; then
-+                            echo "Mounting custom external storage"
-+                            run_as "php /var/www/html/occ files_external:import /opt/nextcloud/custom-mounts.json" || echo "Custom mounts failed"
-+                        fi
-+
-+                        # Auto-create users if provided.
-+                        for f in \$(seq 1 100); do
-+                            username_var="AUTOCREATE_USERNAME\$f"
-+                            password_var="AUTOCREATE_PASSWORD\$f"
-+                            username=\$(eval echo \\\${\$username_var:-})
-+                            password=\$(eval echo \\\${\$password_var:-})
-+                            if [ -n "\${username}" ] && [ -n "\${password}" ]; then
-+                                echo "Creating user \${username}"
-+                                export OC_PASS=\${password}
-+                                run_as "php /var/www/html/occ user:add --password-from-env --group=\"users\" \${username}"
++                            # Enable external storage and create mounts.
++                            run_as "php /var/www/html/occ app:enable files_external"
++                            if [ -f /opt/nextcloud/custom-mounts.json ]; then
++                                echo "Mounting custom external storage"
++                                run_as "php /var/www/html/occ files_external:import /opt/nextcloud/custom-mounts.json" || echo "Custom mounts failed"
 +                            fi
-+                        done
-+                        echo "Finished post-install user initialization"
-                     fi
-                     if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then
-                         echo "setting trusted domains…"
++
++                            # Auto-create users if provided.
++                            for f in \$(seq 1 100); do
++                                username_var="AUTOCREATE_USERNAME\$f"
++                                password_var="AUTOCREATE_PASSWORD\$f"
++                                username=\$(eval echo \\\${\$username_var:-})
++                                password=\$(eval echo \\\${\$password_var:-})
++                                if [ -n "\${username}" ] && [ -n "\${password}" ]; then
++                                    echo "Creating user \${username}"
++                                    export OC_PASS=\${password}
++                                    run_as "php /var/www/html/occ user:add --password-from-env --group=\"users\" \${username}"
++                                fi
++                            done
++                            echo "Finished post-install user initialization"
+                         fi
+                         if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then
+                             echo "Setting trusted domains…"
 EOM
 
 # Run /entrypoint.sh
